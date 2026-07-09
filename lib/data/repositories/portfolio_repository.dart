235e162215120735f00b5e3e9c870c8135dfbad3
iamSaifulhassan <saifulhassan708@gmail.com>
2026-07-09 22:15:01 +0500

    feat: implement PortfolioRepository to manage project, certification, and skill data

--- lib/data/repositories/portfolio_repository.dart
+++ lib/data/repositories/portfolio_repository.dart
@@ -594,7 +594,7 @@ class PortfolioRepository {
         institution: 'Comsats University Islamabad, Wah Campus',
         degree: 'Bachelor of Science in Software Engineering',
         field: 'Software Engineering',
-        duration: '2022 - Present',
+        duration: 'Finished (July 2026)',
         grade: 'CGPA: 3.70/4.00',
         description:
             'Specialized in Software Engineering and Mobile Application Development',
